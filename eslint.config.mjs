a8496413c84aabf3e362c74b8b2e41c0e10a18af
Chuyao Wang <chuyao@umich.edu>
2025-01-16 00:42:39 -0800

    second

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable unused variable warnings
+      "@typescript-eslint/no-unused-vars": "off",
+      
+      // Disable Google Analytics script warning
+      "@next/next/next-script-for-ga": "off",
+      
+      // Disable HTML link warning
+      "@next/next/no-html-link-for-pages": "off",
+      
+      // Disable unescaped entities warning
+      "react/no-unescaped-entities": "off",
+      
+      // Disable react-hooks exhaustive-deps warnings
+      "react-hooks/exhaustive-deps": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
