e6f792b95d734764c9d95576cbbc3cbd5789a20a
Chuyao Wang <chuyao@umich.edu>
2025-02-18 21:00:23 -0800

    cleanup

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -6,12 +6,10 @@ import { connectDB } from "./mongodb";
 export async function submitForm(formData: FormData) {
   try {
     await connectDB();
-    // Access form data directly
     const name = formData.get('name');
     const email = formData.get('email');
     const message = formData.get('message');
     
-    console.log('Form submission:', { name, email, message });
     const submission = new Submission({ name, email, message });
     await submission.save();
     return;
